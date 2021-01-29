52b6d28b312e9f8ed77d3b2d1dce4562a8e3b871
olui1 <ooscarlui97@gmail.com>
2021-01-28 19:51:41 -0500

    initial readme

--- client/src/components/UpdatePosts.jsx
+++ client/src/components/UpdatePosts.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import {Alert} from 'react-bootstrap';
 import PostsFinder from '../apis/PostsFinder';
 
 const UpdatePosts = (props) => {
