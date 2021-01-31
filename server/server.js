251fbc86cb3234790b1449bb8fc5170613ec1663
olui1 <ooscarlui97@gmail.com>
2021-01-31 00:03:42 -0500

    testing

--- server/server.js
+++ server/server.js
@@ -35,12 +35,14 @@ app.use('/api/matchlist', matchList);
 app.use('/api/match', match);
 app.use('/api/leaderboard', leaderboard)
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('../client/build'));
-//     app.get('*', (req,res) => {
-//         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-//     })
-// }
+
+
+if(true){
+    app.use(express.static('build'));
+    app.get('/*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    })
+}
 
 // Create Port
 const port = process.env.PORT || 5000;
