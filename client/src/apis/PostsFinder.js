9ed532a5875409577364b67ef5276e8ae40f473e
olui1 <ooscarlui97@gmail.com>
2022-03-03 18:59:06 -0500

    add / at end

--- client/src/apis/PostsFinder.js
+++ client/src/apis/PostsFinder.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://league-hub.herokuapp.com/api/posts",
+    baseURL: "http://league-hub.herokuapp.com/api/posts/",
 });
