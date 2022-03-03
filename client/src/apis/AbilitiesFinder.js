97071218dad71503f4443ecce0aebde6d3bd65bd
olui1 <ooscarlui97@gmail.com>
2022-03-03 18:33:09 -0500

    reroll changes

--- client/src/apis/AbilitiesFinder.js
+++ client/src/apis/AbilitiesFinder.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://league-hub.herokuapp.com/api/abilities",
+    baseURL: "https://league-hub.herokuapp.com/api/abilities",
 });
