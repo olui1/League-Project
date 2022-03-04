dfa756d3be9e020583ee34c9fb237b4cfdc4464b
olui1 <ooscarlui97@gmail.com>
2022-03-03 19:27:07 -0500

    dealing with CORS

--- client/src/apis/LeaderboardFinder.js
+++ client/src/apis/LeaderboardFinder.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://league-hub.herokuapp.com/api/leaderboard",
+    baseURL: "https://league-hub.herokuapp.com/api/leaderboard",
 });
