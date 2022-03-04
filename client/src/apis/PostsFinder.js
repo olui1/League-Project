cae5e4643a765704f46a084cdc350d23a0ae7215
olui1 <ooscarlui97@gmail.com>
2022-03-03 20:28:46 -0500

    heroku db

--- client/src/apis/PostsFinder.js
+++ client/src/apis/PostsFinder.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://league-hub.herokuapp.com/api/posts/",
+    baseURL: "https://league-hub.herokuapp.com/api/posts",
 });
